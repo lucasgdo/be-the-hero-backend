4e476efd4eb49280d798a09c6911d1bb35c662de
Lucas Gomes de Oliveira <lucasgdo4@gmail.com>
2020-08-21 21:47:57 -0300

    Adding Postgres Compatibility

--- knexfile.js
+++ knexfile.js
@@ -31,17 +31,12 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DB_URL,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
     }
   }
 
